0e2861331cf976bbaadfc7e6809b59e71b2d7ffe
madina myrzakulova <madina.japarbekovna@jmail.com>
2021-08-18 21:45:01 +0300

    init

---
+++ main/views.py
@@ -0,0 +1,14 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+
+
+
+
+
+def homepage(request):
+    return HttpResponse("Hello world")
+
+def test(request):
+    return render(request, "test.html")    
+
+
